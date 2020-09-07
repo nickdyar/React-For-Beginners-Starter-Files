221eac7486338f5b0d03e1f702fd6e3e3b21c47e
nickdyar <nick.dyar@gmail.com>
2020-09-07 15:35:34 -0400

    Initial commit.

---
+++ catch-of-the-day/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+class StorePicker extends React.Component {
+	render() {
+		return <p>Hello</p>
+	}
+}
+
+
+render(<StorePicker/>, document.querySelector('main'));
