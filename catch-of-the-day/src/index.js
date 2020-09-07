247658a8356f016ccd245a64fed944544fe97b20
nickdyar <nick.dyar@gmail.com>
2020-09-07 15:59:36 -0400

    Create StorePicker form component.

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,11 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
 
-class StorePicker extends React.Component {
-	render() {
-		return <p>Hello</p>
-	}
-}
-
-
-render(<StorePicker/>, document.querySelector('main'));
+render(<StorePicker/>, document.querySelector('#main'));
